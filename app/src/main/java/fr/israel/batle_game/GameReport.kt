ee0a9d82dad67c50a2b4cfc97d59661d7becd900
Rapha <rapha.israel@live.fr>
2022-05-30 17:49:46 +0200

    initialize project and create 3 screen

---
+++ app/src/main/java/fr/israel/batle_game/GameReport.kt
@@ -0,0 +1,11 @@
+package fr.israel.batle_game
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameReport : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_reports)
+    }
+}
---
+++ app/src/main/java/fr/israel/batle_game/NewGame.kt
@@ -0,0 +1,11 @@
+package fr.israel.batle_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewGame : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.new_game)
+    }
+}

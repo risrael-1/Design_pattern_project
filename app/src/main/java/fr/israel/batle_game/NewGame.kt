4306247d109e54bfe4e7fa9c45abb9c28c074abd
risrael-1 <rapha.israel@live.fr>
2022-07-05 15:23:04 +0200

    add startGame view

--- app/src/main/java/fr/israel/batle_game/NewGame.kt
+++ app/src/main/java/fr/israel/batle_game/NewGame.kt
@@ -1,7 +1,10 @@
 package fr.israel.batle_game
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class NewGame : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +17,12 @@ class NewGame : AppCompatActivity() {
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
+
+        val startGame: Button = findViewById(R.id.buttonContinue)
+        startGame.setOnClickListener {
+            val intent = Intent(this, StartGame::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {

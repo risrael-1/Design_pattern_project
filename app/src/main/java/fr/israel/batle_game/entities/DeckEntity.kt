3aa56169233bdf48a01a66b35959bdefb111ba7f
risrael-1 <rapha.israel@live.fr>
2022-07-27 20:44:28 +0200

    add deck players

--- app/src/main/java/fr/israel/batle_game/entities/DeckEntity.kt
+++ app/src/main/java/fr/israel/batle_game/entities/DeckEntity.kt
@@ -1,4 +1,33 @@
 package fr.israel.batle_game.entities
 
+import fr.israel.batle_game.R
+
 class DeckEntity {
+    val deck1 = mapOf(
+
+        "1" to R.drawable.ic_noun_1_carreau,
+        "2" to R.drawable.ic_noun_2_carreau,
+        "3" to R.drawable.ic_noun_3_carreau,
+        "4" to R.drawable.ic_noun_4_pique,
+        "5" to R.drawable.ic_noun_5_treffle,
+        "6" to R.drawable.ic_noun_6_carreau,
+        "7" to R.drawable.ic_noun_7_carreau,
+        "8" to R.drawable.ic_noun_8_pique,
+        "9" to R.drawable.ic_noun_9_carreau,
+        "10" to R.drawable.ic_noun_10_coeur,
+    )
+
+    val deck2 = mapOf(
+
+        "1" to R.drawable.ic_noun_1_pique,
+        "2" to R.drawable.ic_noun_2_reine,
+        "3" to R.drawable.ic_noun_3_pique,
+        "4" to R.drawable.ic_noun_4_treffle,
+        "5" to R.drawable.ic_noun_5_pique,
+        "6" to R.drawable.ic_noun_6_pique,
+        "7" to R.drawable.ic_noun_7_pique,
+        "8" to R.drawable.ic_noun_8_carreau,
+        "9" to R.drawable.ic_noun_9_pique,
+        "10" to R.drawable.ic_noun_10_carreau,
+    )
 }
